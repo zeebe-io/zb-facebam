1d60da0dd541e3f472a1c55db322a3bb8c1d5ea8
jsam <samuel.picek@camunda.com>
2017-09-19 09:33:19 +0200

    cleanup()

--- board/svc.go
+++ board/svc.go
@@ -9,8 +9,46 @@ import (
 	"io/ioutil"
 	"github.com/davecgh/go-spew/spew"
 	"time"
+	"net/http"
 	//todo: remove
 )
+
+type WrappedResponseWriter struct {
+	gin.ResponseWriter
+	writer http.ResponseWriter
+}
+
+func (w *WrappedResponseWriter) Write(data []byte) (int, error) {
+	return w.writer.Write(data)
+}
+
+func (w *WrappedResponseWriter) WriteString(s string) (n int, err error) {
+	return w.writer.Write([]byte(s))
+}
+
+
+type NextRequestHandler struct{
+	c *gin.Context
+}
+
+// Run the next request in the middleware chain and return
+// See: https://godoc.org/github.com/gin-gonic/gin#Context.Next
+func (h *NextRequestHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	h.c.Writer = &WrappedResponseWriter{h.c.Writer, w}
+	h.c.Next()
+}
+
+// Wrap something that accepts an http.Handler, returns an http.Handler
+func WrapHH(hh func(h http.Handler) http.Handler) gin.HandlerFunc {
+	// Steps:
+	// - create an http handler to pass `hh`
+	// - call `hh` with the http handler, which returns a function
+	// - call the ServeHTTP method of the resulting function to run the rest of the middleware chain
+
+	return func(c *gin.Context) {
+		hh(&NextRequestHandler{c}).ServeHTTP(c.Writer, c.Request)
+	}
+}
 
 type Board struct {
 	client *zbc.Client
@@ -27,10 +65,13 @@ func Run() {
 	r := gin.Default()
 	board := NewBoard()
 
-	r.GET("/", func(c *gin.Context) {
+	r.GET("/", gin.WrapH(http.FileServer(http.Dir("/tmp/watermarking"))))
+
+	r.GET("/upload", func(c *gin.Context) {
 		c.File("board/templates/upload.html")
 	})
 
+		//gin.WrapH(http.FileServer(http.Dir("/tmp"))))
 	r.POST("/upload", func(c *gin.Context) {
 		img, err := imageupload.Process(c.Request, "file")
 
